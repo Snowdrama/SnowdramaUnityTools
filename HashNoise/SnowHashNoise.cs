9eea02ba0d917d5090e95b9c5a39e499f522877d
John 'Snow' Close <SnowdramaGames@gmail.com>
2021-10-29 00:37:47 -0400

    fixes and cleanup

--- HashNoise/SnowHashNoise.cs
+++ HashNoise/SnowHashNoise.cs
@@ -2,61 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnowHash : MonoBehaviour
+namespace Snowdrama
 {
-    /// <summary>
-    /// A C# implementatiion of the Squirrel3 hash function from:
-    /// Squirrel Eiserloh's GDC Talk 
-    /// https://www.youtube.com/watch?v=LWFzPP8ZbdU
-    /// </summary>
-    /// <param name="position">Some positional value in the noise array</param>
-    /// <param name="seed">The noise functions seed</param>
-    /// <returns></returns>
-    public uint Squirrel3(int position, uint seed)
+    public class SnowHash : MonoBehaviour
     {
-        const uint NOISE_1 = 0xB5297A4D;
-        const uint NOISE_2 = 0x68E31DA4;
-        const uint NOISE_3 = 0x1B56C4E9;
+        /// <summary>
+        /// A C# implementatiion based on the example hash function
+        /// in Squirrel Eiserloh's GDC Talk 
+        /// https://www.youtube.com/watch?v=LWFzPP8ZbdU
+        /// </summary>
+        /// <param name="position">Some positional value in the noise array</param>
+        /// <param name="seed">The noise functions seed</param>
+        /// <returns></returns>
+        public uint Squirrel3(int position, uint seed = 69420)
+        {
+            const uint NOISE_1 = 0xB5297A4D;
+            const uint NOISE_2 = 0x68E31DA4;
+            const uint NOISE_3 = 0x1B56C4E9;
 
-        uint mangled = (uint)position;
-        mangled *= NOISE_1;
-        mangled += seed;
-        mangled ^= mangled >> 8;
-        mangled *= NOISE_2;
-        mangled ^= mangled << 8;
-        mangled *= NOISE_3;
-        mangled ^= mangled >> 8;
-        return mangled;
+            uint mangled = (uint)position;
+            mangled *= NOISE_1;
+            mangled += seed;
+            mangled ^= mangled >> 8;
+            mangled *= NOISE_2;
+            mangled ^= mangled << 8;
+            mangled *= NOISE_3;
+            mangled ^= mangled >> 8;
+            return mangled;
+        }
+
+
+        /// <summary>
+        /// A modified 64 bit version of the Squirrel3 hash function from:
+        /// Squirrel Eiserloh's GDC Talk 
+        /// https://www.youtube.com/watch?v=LWFzPP8ZbdU
+        /// 
+        /// Totally untested on how random it actually is, how performant it is
+        /// or how quality the random is.
+        /// 
+        /// This function is intended to be a joke... unless it works... :P
+        /// </summary>
+        /// <param name="position">Some positional value in the noise array</param>
+        /// <param name="seed">The noise functions seed</param>
+        /// <returns></returns>
+        public long Snow64(int position, long seed = 69420717580085)
+        {
+            const long NOISE_1 = 0xB5297A4D;
+            const long NOISE_2 = 0x68E31DA4;
+            const long NOISE_3 = 0x1B56C4E9;
+
+            long mangled = (long)position;
+            mangled *= NOISE_1;
+            mangled += seed;
+            mangled ^= mangled >> 16;
+            mangled *= NOISE_2;
+            mangled ^= mangled << 16;
+            mangled *= NOISE_3;
+            mangled ^= mangled >> 16;
+            return mangled;
+        }
     }
 
-
-    /// <summary>
-    /// A modified 64 bit version of the Squirrel3 hash function from:
-    /// Squirrel Eiserloh's GDC Talk 
-    /// https://www.youtube.com/watch?v=LWFzPP8ZbdU
-    /// 
-    /// Totally untested on how random it actually is, how performant it is
-    /// or how quality the random is.
-    /// 
-    /// This function is intended to be a joke... unless it works... :P
-    /// </summary>
-    /// <param name="position">Some positional value in the noise array</param>
-    /// <param name="seed">The noise functions seed</param>
-    /// <returns></returns>
-    public long Snow64(int position, long seed = 69420717580085)
-    {
-        const long NOISE_1 = 0xB5297A4D;
-        const long NOISE_2 = 0x68E31DA4;
-        const long NOISE_3 = 0x1B56C4E9;
-
-        long mangled = (long)position;
-        mangled *= NOISE_1;
-        mangled += seed;
-        mangled ^= mangled >> 16;
-        mangled *= NOISE_2;
-        mangled ^= mangled << 16;
-        mangled *= NOISE_3;
-        mangled ^= mangled >> 16;
-        return mangled;
-    }
 }
